7d628a9d64c26bb515fe8cfacf54585170656e29
coderxcore <rayhicks2000@gmail.com>
2026-01-13 15:00:54 +0800

    完成表情搜索

--- src/background/search/searchEmoji.ts
+++ src/background/search/searchEmoji.ts
@@ -9,6 +9,13 @@ export function searchEmoji(query: string): ISearchTerm[] {
 		return [];
 	}
 	checkAndInitMap();
+	const lastCode = query.charCodeAt(query.length - 1);
+	if (!lastCharMap.has(lastCode)) {
+		return [];
+	}
+	const descArr = lastCharMap.get(lastCode)!;
+	const result = descArr.filter(desc => query.endsWith(desc));
+	return result.map(desc => descEmojiMap.get(desc)!).map(text => ({text})) as any;
 }
 
 function checkAndInitMap() {
@@ -20,21 +27,24 @@ function checkAndInitMap() {
 	const dict = FileData.emojisDict()
 	for (let i = 0; i < dict.length; i++) {
 		const [emoji, descriptions] = dict[i];
-		for(let j = 0; j < descriptions.length; j++) {
-			const desc = descriptions[j];
+		for (let j = 0; j < descriptions.length; j++) {
+			const desc = descriptions[j].toLowerCase();
 
-			const emojiArr = descEmojiMap.get(desc)||[];
-			emojiArr.push(emoji);
+			const emojiArr = descEmojiMap.get(desc) || [];
+			if (!emojiArr.includes(emoji)) {
+				emojiArr.push(emoji);
+			}
 			descEmojiMap.set(desc, emojiArr);
 
 			const lastChar = desc.charCodeAt(desc.length - 1);
-			const discArr = lastCharMap.get(lastChar)||[];
-			discArr.push(desc);
+			const discArr = lastCharMap.get(lastChar) || [];
+			if (!discArr.includes(desc)) {
+				discArr.push(desc);
+			}
 			lastCharMap.set(lastChar, discArr);
+		}
+		for (const arr of lastCharMap.values()) {
+			arr.sort((a, b) => b.length - a.length);
 		}
 	}
 }
-
-checkAndInitMap();
-console.log(descEmojiMap)
-console.log(lastCharMap)
